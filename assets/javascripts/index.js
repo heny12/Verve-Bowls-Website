549c3c7fa50094e2b0555b5e4ffedf65408e9610
Henry Chipman <hdchipman@msn.com>
2016-05-31 12:10:09 -0700

    updates

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	// smooth scrolling to anchors
 	$('a').click(function(){
 	    $('html, body').animate({
-	        scrollTop: $( $(this).attr('href') ).offset().top - 40
+	        scrollTop: $( $(this).attr('href') ).offset().top - 55
 	    }, 500);
 	    return false;
 	});
