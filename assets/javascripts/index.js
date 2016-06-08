b1ddf2c114dfd00951b7bba9364b2b55c786c2de
Henry Chipman <hdchipman@msn.com>
2016-06-08 11:41:38 -0700

    added maps and contact form

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -7,3 +7,30 @@ $( document ).ready(function() {
 	    return false;
 	});
 });
+
+
+// initialize map
+function initializeMap() {
+	var center = new google.maps.LatLng(47.6152372,-122.3227216);
+	var map = document.getElementById("map");
+	var pano = document.getElementById("pano");
+    var map = new google.maps.Map(map,{
+        zoom: 12,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
+    });
+	var panorama = new google.maps.StreetViewPanorama(pano,{
+	    position: center,
+	    scrollwheel: false,
+        addressControl: false,
+	    pov: {
+	      heading: 180,
+	      pitch: 5,
+	      zoom: 0
+	    }
+	  });
+	map.setStreetView(panorama);
+}
+google.maps.event.addDomListener(window, "load", initializeMap);
+
